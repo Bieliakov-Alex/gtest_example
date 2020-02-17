323a809df76772e992723132b60c4b00a6d38a20
Oleksandr Bieliakov <oleksandr.bieliakov@volvocars.com>
2020-02-17 15:40:16 +0200

    Vanila example
    I took this example from
    https://raymii.org/s/tutorials/Cpp_project_setup_with_cmake_and_unit_tests.html

---
+++ src/Formula.h
@@ -0,0 +1,9 @@
+#ifndef EXAMPLEGTESTPROJECT_FORMULA_H_
+#define EXAMPLEGTESTPROJECT_FORMULA_H_
+
+class Formula {
+public:
+  static int bla(int arg1);
+};
+
+#endif // EXAMPLEGTESTPROJECT_FROMULA_H_
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+/*Copyright 2020 Alex Bieliakov*/
+
+#include "Formula.h"
+#include <iostream>
+
+int main() {
+  std::cout << "Bla: " << Formula::bla(2) << std::endl;
+  return 0;
+}
